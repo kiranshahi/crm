547ecf0680375526b0b03ceb56d75e46f90a44f8
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-03-01 22:38:26 +0545

    Mongodb connection done.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhostCRMdb', {
+    useMongoClient: true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
